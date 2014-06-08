3473cd863c91bb7e5db219204bfa304034f9dfc6
simplej978 <Jmerrill@John.(none)>
2014-06-08 15:11:44 -0400

    dis IDE doe

--- Sharp2D/Sharp2D/Core/Settings/Settings.cs
+++ Sharp2D/Sharp2D/Core/Settings/Settings.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,26 @@ using System.Threading.Tasks;
 
 namespace Sharp2D.Core.Settings
 {
-    public class Settings
+    public abstract class Settings
     {
 
+        public abstract void OnLoad();
+        public abstract void OnSave();
+        
+        public static T Load<T>(string Path) where T : Settings{
+            if (!System.IO.File.Exists(Path))
+            {
+                return (T)Activator.CreateInstance(typeof(T));
+            }
+            
+            T settings = JsonConvert.DeserializeObject<T>(Path);
+            settings.OnLoad();
+            return settings;
+        }
+
+        public static void Save(Settings Settings, string Path){
+            Settings.OnSave();
+            System.IO.File.WriteAllText(Path, JsonConvert.SerializeObject(Settings, Formatting.Indented));   
+        }
     }
 }
