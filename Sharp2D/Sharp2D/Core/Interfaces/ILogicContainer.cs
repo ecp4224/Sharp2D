007c32e02edcfeef4b84daea972f01a99cee33be
Eddie Penta <ecp4224@gmail.com>
2015-02-28 16:39:54 -0500

    Add "Add and Remove" methods to ILogicalContainer and IRenderJobContainer

--- Sharp2D/Sharp2D/Core/Interfaces/ILogicContainer.cs
+++ Sharp2D/Sharp2D/Core/Interfaces/ILogicContainer.cs
@@ -11,5 +11,7 @@ namespace Sharp2D.Core.Interfaces
         void PreFetch(); //Warn before fetching
         List<ILogical> LogicalList { get; } //Fetch
         void PostFetch(); //Inform the fetch is complete
+        void AddLogical(ILogical logical); //Add a logical to the LogicalList
+        void RemoveLogical(ILogical logical); //Remove a logical from the LogicalList
     }
 }
